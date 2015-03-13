c3187975dd7710dcb389806b47f452bf914f81bb
Brian Mascarenhas <brian@brianmascarenhas.com>
2015-03-12 22:11:35 -0700

    fix optional params

--- lib/plaid/call.rb
+++ lib/plaid/call.rb
@@ -129,7 +129,8 @@ module Plaid
         :access_token => access_token,
         :upgrade_to => resource,
       }
-      payload.merge!(options) if options
+      payload.merge!(:options => options) if options
+      puts ">>>>>>>>>>> #{payload}"
       payload
     end
 
